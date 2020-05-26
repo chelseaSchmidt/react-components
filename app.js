9d608fb09ac8c15b38c5e8fb88d8ffe02914b7e4
Chelsea Schmidt <cschmidt331@gmail.com>
2020-05-26 11:51:55 -0700

    Experimented with nested components and props

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 // TODO
-let GroceryList = () => ( /*Why does this use a paren instead of a curly brace*/
+let GroceryList = () => ( /*parens are wrapping returned JSX*/
   <ul>
-    <Coffee />
-    <Tea />
+    <GroceryListItem item = {'Coffee'}/> {/*This creates an instance of the Coffee component*/}
+    <GroceryListItem item = {'Tea'}/>
   </ul>
+);
+
+let GroceryList2 = () => (
+  <GroceryListItems items = {['Coffee', 'Tea']}/>
 );
 
 let Coffee = () => (
@@ -14,4 +18,19 @@ let Tea = () => (
   <li>Tea</li>
 );
 
-ReactDOM.render(<GroceryList />, document.getElementById('app'));
+let GroceryListItem = (props) => (
+  <li>{props.item}</li>
+);
+
+let GroceryListItems = (props) => (
+  <ul>
+    <li>{props.items[0]}</li>
+    <li>{props.items[1]}</li>
+  </ul>
+);
+
+//props is a convential name for the object that gets passed in when creating a new instance of a
+//component; you define properties on the object by 'item = {...}' and would access that
+//property via props.item
+
+ReactDOM.render(<GroceryList />, document.getElementById('app')); /*This creates an instance of the GroceryList component and renders it to the page */
