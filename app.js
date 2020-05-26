d161e7ee23215c23e8416daa6d96352b30f9a76d
Chelsea Schmidt <cschmidt331@gmail.com>
2020-05-26 11:07:11 -0700

    Created and rendered GroceryList React component

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 // TODO
+let GroceryList = () => ( /*Why does this use a paren instead of a curly brace*/
+  <ul>
+    <li>Coffee</li>
+    <li>Tea</li>
+  </ul>
+);
+
+let Coffee = () => (
+  <li>Coffee</li>
+);
+
+let Tea = () => (
+  <li>Tea</li>
+);
+
+ReactDOM.render(<GroceryList />, document.getElementById('app'));
